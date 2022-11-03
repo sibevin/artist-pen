016a8350fd26bd91cd0b7a9a239dff6bb88a42c4
Kait Wang <sibevin@gmail.com>
2022-11-03 19:19:37 +0800

    Setup tailwind css and daisyui

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from "./components/HelloWorld.vue";
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <button class="btn btn-primary">Test</button>
 </template>
 
 <style scoped>
